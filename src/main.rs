46cf15a6fe11871eae9fd45029fdcc434f7e3b07
Samuel Barbeau <samuel.barbeau@powerhealthsolutions.com>
2023-05-07 22:53:37 -0400

    feat: map render in main

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,28 @@ mod prelude {
 
 use prelude::*;
 
-struct State {}
+struct State {
+    map: Map,
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Gobekli")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!")
+        self.map.render(ctx)
     }
-}
-
-fn main() -> BError {
-    let context = BTermBuilder::simple80x50().with_title("Gobekli").build()?;
-
-    main_loop(context, State {})
 }
