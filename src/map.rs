b458867f5fac8002f704a6dac6bae52ceb336863
Samuel Barbeau <samuel.barbeau@powerhealthsolutions.com>
2023-05-09 21:53:26 -0400

    feat: adds player and renders it in tick

--- src/map.rs
+++ src/map.rs
@@ -34,20 +34,17 @@ impl Map {
             }
         }
     }
-    pub fn in_bound(&self, point: Point) -> bool {
-        point.x >= 0
-            && point.x < SCREEN_WIDTH
-            && point.y >= SCREEN_HEIGHT
-            && point.y < SCREEN_HEIGHT
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
     }
     pub fn can_enter_tile(&self, point: Point) -> bool {
-        self.in_bound(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
     }
     pub fn try_idx(&self, point: Point) -> Option<usize> {
-      if !self.in_bound(point){
-        None
-      } else {
-        Some(map_idx(point.x, point.y))
-      }
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
+        }
     }
 }
